204dfa22eab89e4edd6f50dc5e92108d8bc7f80e
Surya_23 <155769533+Surya060405@users.noreply.github.com>
2025-05-08 18:24:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def predict():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
